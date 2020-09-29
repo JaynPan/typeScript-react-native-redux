0875a6e8775650e1c24eee96d9c73f4f10c811a4
Jay Pan <lb0104672@gmail.com>
2020-09-29 16:04:00 +0800

    ESLint

--- App.tsx
+++ App.tsx
@@ -1,15 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { FC } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
 const styles = StyleSheet.create({
   container: {
@@ -19,3 +10,14 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+const App: FC = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+};
+
+export default App;
